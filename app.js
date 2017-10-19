1c0e050030e7604083cd91e80053394e76537790
Kevin Springer <KevinUSCU@outlook.com>
2017-10-19 16:05:53 -0700

    Update server code.

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ app.use(bodyParser.json())
 const morgan = require('morgan')
 app.use(morgan('dev'))
 
+const trollsRoutes = require('./src/routes/trolls-routes')
+app.use('/trolls', trollsRoutes)
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({ error: err })
+})
 
-
-
-
-
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
+})
 
 const listener = () => console.log(`Listening on port ${port}`)
 app.listen(port, listener)
